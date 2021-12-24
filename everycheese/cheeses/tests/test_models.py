9c7c1441d5f138ef4a9b628ff0315505c39bca7d
WF | Teodora Stefanovska <teodora.stefanovska@webfactory.mk>
2021-12-24 13:41:28 +0100

    Use CheeseFactory in a tesr

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,16 +1,13 @@
 import pytest
 
-pytestmark = pytest.mark.django_db
-
 from ..models import Cheese
+from .factories import CheeseFactory
+
+pytestmark = pytest.mark.django_db
 
 
 def test__str__():
     """Test string representation of Cheese"""
-    cheese = Cheese.objects.create(
-        name='Stracchino',
-        description='Semi-sweet cheese',
-        firmness=Cheese.Firmness.SOFT
-    )
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
